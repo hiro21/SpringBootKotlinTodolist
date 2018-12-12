efeea836ca4fa67bfdce1fe13271d007b750ca51
hiro21 <hiswing8@gmail.com>
2018-12-13 00:07:50 +0900

    タスク一覧を表示する対応

--- src/main/kotlin/com/example/todolist/TaskController.kt
+++ src/main/kotlin/com/example/todolist/TaskController.kt
@@ -1,18 +1,20 @@
 package com.example.todolist
 
+import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
 
-@RestController
+@Controller
 @RequestMapping("tasks")
 class TaskController {
     @GetMapping("")
-    fun index(): String {
+    fun index(model: Model): String {
         val tasks = listOf(
                 Task(1, "test1", false),
                 Task(2, "test2", true)
         )
-        return tasks.toString()
+        model.addAttribute("tasks", tasks)
+        return "tasks/index"
     }
 }
